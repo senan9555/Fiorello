58ca983c3672ca75baaa5c487e74d0aee0467871
senan9555 <sananraa139@gmail.com>
2022-09-12 12:41:01 +0400

    Create Category CRUD

---
+++ Fiorello/Fiorello/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,111 @@
+﻿using Fiorello.DAL;
+using Fiorello.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Fiorello.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _db;
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Category> categories = await _db.Categories.ToListAsync();
+            return View(categories);
+        }
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            bool isExist = await _db.Categories.AnyAsync(x => x.Name == category.Name);
+            if (isExist)
+            {
+                ModelState.AddModelError("Title", "This service already is exist");
+                return View();
+            }
+            await _db.Categories.AddAsync(category);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Category dbCategory = await _db.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (dbCategory == null)
+            {
+                return BadRequest();
+            }
+            return View(dbCategory);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int? id, Category category)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Category dbCategory = await _db.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (dbCategory == null)
+            {
+                return BadRequest();
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(dbCategory);
+            }
+            bool isExist = await _db.Categories.AnyAsync(x => x.Name == category.Name && x.Id != id);
+            if (isExist)
+            {
+                ModelState.AddModelError("Title", "This service already is exist");
+                return View();
+            }
+            dbCategory.Name = category.Name;
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Activity(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Category category = await _db.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (category == null)
+            {
+                return BadRequest();
+            }
+            if (category.IsDeactive)
+            {
+                category.IsDeactive = false;
+            }
+            else
+            {
+                category.IsDeactive = true;
+            }
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
